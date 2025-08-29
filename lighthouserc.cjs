963b67e5b76ec7970d862aba1cfda3c532d6d496
Mubashar Shakeel <moonmubashar945@gmail.com>
2025-08-29 17:09:13 +0500

    fix: Updated foce-dynamic

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -5,8 +5,7 @@ module.exports =  {
       collect: {
         url: [process.env.LHCI_URL || "http://localhost:3000"],
         startServerCommand: process.env.LHCI_START || undefined,
-        staticDistDir:'./.next/server/app',
-        
+  
         settings: {
           onlyCategories: [
             'performance',
